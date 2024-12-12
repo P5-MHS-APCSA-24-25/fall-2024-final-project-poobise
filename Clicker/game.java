96db086aebcc81e508c75c62cbf28c2cb9aa385b
Matthew Tuan <matthewtuan210034@gmail.com>
2024-12-12 12:34:22 -0600

    basically done

--- Clicker/game.java
+++ Clicker/game.java
@@ -6,37 +6,41 @@ import java.util.Scanner;
 
 public class game {
     public static void main(String[] args) {
-        Items firstOne = new Items("firstone","On Hit",2,0)
+        Items firstOne = new Items("firstone","On Hit",1,"add",0,"b",3,"1 more hit per letter");
 
         Scanner input = new Scanner(System.in);
-        checkInput plop = new checkInput("helo");
+        checkInput checker = new checkInput("helo");
 
-        Double hits = 0.0;
+        double hits = 0.0; // change back to capital D if something goes wrong
         boolean shopOpen = false;
 
         while (true) {
             String userInput = input.next();
 
             if (userInput != "") {
-                plop.setInput(userInput);
-                String checkedInput = plop.checkInput();
+                checker.setInput(userInput);
+                String checkedInput = checker.checkInput();
 
                 if (checkedInput.equals("shop")) {
-                    shopOpen = true;
-                    plop.toggleShop();
+                    shopOpen = !shopOpen;
+                    checker.toggleShop();
                 } else if (checkedInput.matches(".*\\d.*")) {
-                    hits += Double.parseDouble(checkedInput);
+                    hits += firstOne.activateHitItems(Double.parseDouble(checkedInput));
                 } else if (shopOpen) {
-                    if (checkedInput.equals("b")) {
-
+                    if (checkedInput.equals(firstOne.buyKey)) {
+                        firstOne.buyItem(firstOne,hits);
+                        firstOne.setItemsFolder();
                     }
                 }
             }
 
             if (shopOpen) {
-                System.out.println("WELCOME TO THE SHOP!")
-                System.out.println("current items are: ")
-                System.out.println(firstOne)
+                System.out.println("WELCOME TO THE SHOP!");
+                System.out.println("current items are: ");
+                if (firstOne.checkItemStatus())
+                    System.out.println(firstOne.name+": "+firstOne.description+"\nPrice is: "+firstOne.price);
+                else
+                    System.out.println("Item bought");
             } else
                 System.out.println("Hits: "+hits);
         }   
--- Clicker/items.java
+++ Clicker/items.java
@@ -3,8 +3,9 @@
 // Period: 5A
 
 public class Items {
-    private Items[] inventory = {};
-    private Items[] hitItems = {};
+    private Items[] inventory = new Items[1];
+    private Items[] hitItems = new Items[1];
+
     public String name;
     public String itemType;
     public int value;
@@ -12,45 +13,70 @@ public class Items {
     public double time;
 
     public String buyKey;
-    public int price;
+    public double price;
+    public String description;
 
-    public Items(String gN, String gIT, int gV, String gUT, double gTi, String gBK, int gP) {
+    public Items(String gN, String gIT, int gV, String gUT, double gTi, String gBK, int gP, String gD) {
         name = gN;
         itemType = gIT;
         value = gV;
         upgradeType = gUT;
-        time = gTi
+        time = gTi;
         buyKey = gBK;
         price =gP;
+        description = gD;
     }
 
-    public void checkItems() {
-        for (i=1,i <= inventory.size(),i++) {
-            private Items currentItem = inventory[i];
+    public void setItemsFolder() {
+        for (int i=1; i < inventory.length; i++) {
+            Items currentItem = inventory[i];
             if (currentItem.itemType.equals("On Hit") && currentItem.time == 0) {
-                hitItems = addToItemArray(hitItems, checkItems);
+                hitItems = addToItemArray(hitItems, currentItem);
             } else if (currentItem.itemType.equals("Automatic")) {
 
             }
         }
     }
 
-    public void activateHitItems() {
-        double total = 0;
-        for (i=1,i >=hitItems.size(),i++) {
-            total = hitItems[i].value;
+    public double activateHitItems(double letters) {
+        double totalMulti = 1;
+        for (int i=1; i < hitItems.length; i++) {
+            Items currentItem = hitItems[i];
+            System.out.println(currentItem.upgradeType);
+            if (currentItem.upgradeType.equals("add"))
+                letters += currentItem.value;
+            else if (currentItem.upgradeType.equals("multi"))
+                totalMulti *= currentItem.value;
         }
+        return letters*totalMulti;
+    }
+
+    public void buyItem(Items itemToBuy, double currentHits) {
+        if (currentHits >= itemToBuy.price) {
+            currentHits -= itemToBuy.price;
+            inventory = addToItemArray(inventory, itemToBuy);
+        }
+    } 
+
+    public boolean checkItemStatus() {
+        for (int i=1; i < inventory.length; i++) {
+            Items currentItem = inventory[i];
+            if (currentItem.name.equals(name)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private Items[] addToItemArray(Items[] array, Items itemToAdd) {
 
-        Items[] newArray = Items[array.size()];
+        Items[] newArray = new Items[array.length+1];
 
-        for (int i = 1; i =< array.size(),i++) {
+        for (int i = 1; i < array.length; i++) {
             newArray[i] = array[i];
         }
 
-        newArray[array.size()+1] = itemToAdd;
+        newArray[array.length] = itemToAdd;
 
         return newArray;
     }
